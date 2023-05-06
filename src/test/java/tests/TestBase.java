4643eebf1d47e6b8006aea113aa1eeb9b9e4a476
Teymur Baratov <baratov.teymur@yandex.ru>
2023-05-06 16:27:36 +0300

    add selenoid config

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,8 +7,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.time.Duration;
+import java.util.Map;
 
 public class TestBase {
 
@@ -19,6 +21,15 @@ public class TestBase {
         WebDriverManager.chromedriver().setup();
         Configuration.baseUrl = baseUrl;
         Configuration.browserSize = browserSize;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+
 //        Configuration.pageLoadTimeout = 15000;
 //        Configuration.browser = "fireFox";
         Configuration.pageLoadStrategy = "eager";
