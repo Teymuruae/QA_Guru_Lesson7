7bab4e8c8e00fe50d3372cd2da6e109aab4b5422
Teymur Baratov <baratov.teymur@yandex.ru>
2023-05-08 19:04:55 +0300

    remote configs

--- src/test/java/tests/DemoQaTest.java
+++ src/test/java/tests/DemoQaTest.java
@@ -1,14 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
 import com.github.javafaker.Faker;
-import enums.*;
+import enums.ModalDialogFields;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import testdata.TestData;
-
-import java.util.Locale;
 
 import static io.qameta.allure.Allure.step;
 
@@ -28,48 +25,44 @@ public class DemoQaTest extends TestBase {
     @Test
     public void fillFormTest() {
 
-     step("open registration page", ()->{
-    registrationPage
-            .openPage()
-            .removeDomElements();
-});
-     step("fill registration form",() -> {
-         registrationPage
-                 .fillFirstNameField(TestData.firstName)
-                 .fillLastNameField(TestData.lastName)
-                 .fillEmailField(TestData.email)
-                 .fillPhoneNumberField(TestData.phoneNumber)
-                 .setGender(TestData.gender)
-                 .chooseSubject(TestData.subject)
-                 .chooseState(TestData.state)
-                 .chooseCity(city)
-                 .fillTextArea(TestData.textAreaText)
-                 .chooseHobby(TestData.hobby)
-                 .uploadImage(imageToUploadPath)
-                 .setBirthDate(TestData.birthDay, TestData.month, TestData.birthYear)
-                 .clickSubmitButton();
-     });
-     step("verify form", ()->{
-         registrationPage
-                 .verifyModalDialogAppear()
-                 .verifyModalDialogsElements(key.getStudenName(), TestData.firstName + " " + TestData.lastName)
-                 .verifyModalDialogsElements(key.getStudentEmail(), TestData.email)
-                 .verifyModalDialogsElements(key.getMobile(), TestData.phoneNumber)
-                 .verifyModalDialogsElements(key.getDateOfBirth(), String
-                         .format( "%s %s,%s", TestData.birthDay, TestData.month, TestData.birthYear))
-                 .verifyModalDialogsElements(key.getGender(), TestData.gender)
-                 .verifyModalDialogsElements(key.getSubjects(), TestData.subject)
-                 .verifyModalDialogsElements(key.getStateAndCity(), String
-                         .format("%s %s",TestData.state, city  ))
-                 .verifyModalDialogsElements(key.getHobbies(), TestData.hobby)
-                 .verifyModalDialogsElements(key.getPicture(), expectedUploadFileName)
-                 .verifyModalDialogsElements(key.getAddress(), TestData.textAreaText);
-     });
-
-
+        step("open registration page", () -> {
+            registrationPage
+                    .openPage()
+                    .removeDomElements();
+        });
+        step("fill registration form", () -> {
+            registrationPage
+                    .fillFirstNameField(TestData.firstName)
+                    .fillLastNameField(TestData.lastName)
+                    .fillEmailField(TestData.email)
+                    .fillPhoneNumberField(TestData.phoneNumber)
+                    .setGender(TestData.gender)
+                    .chooseSubject(TestData.subject)
+                    .chooseState(TestData.state)
+                    .chooseCity(city)
+                    .fillTextArea(TestData.textAreaText)
+                    .chooseHobby(TestData.hobby)
+                    .uploadImage(imageToUploadPath)
+                    .setBirthDate(TestData.birthDay, TestData.month, TestData.birthYear)
+                    .clickSubmitButton();
+        });
+        step("verify form", () -> {
+            registrationPage
+                    .verifyModalDialogAppear()
+                    .verifyModalDialogsElements(key.getStudenName(), TestData.firstName + " " + TestData.lastName)
+                    .verifyModalDialogsElements(key.getStudentEmail(), TestData.email)
+                    .verifyModalDialogsElements(key.getMobile(), TestData.phoneNumber)
+                    .verifyModalDialogsElements(key.getDateOfBirth(), String
+                            .format("%s %s,%s", TestData.birthDay, TestData.month, TestData.birthYear))
+                    .verifyModalDialogsElements(key.getGender(), TestData.gender)
+                    .verifyModalDialogsElements(key.getSubjects(), TestData.subject)
+                    .verifyModalDialogsElements(key.getStateAndCity(), String
+                            .format("%s %s", TestData.state, city))
+                    .verifyModalDialogsElements(key.getHobbies(), TestData.hobby)
+                    .verifyModalDialogsElements(key.getPicture(), expectedUploadFileName)
+                    .verifyModalDialogsElements(key.getAddress(), TestData.textAreaText);
+        });
 
 
     }
-
-
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,6 +7,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.time.Duration;
@@ -14,32 +15,40 @@ import java.util.Map;
 
 public class TestBase {
 
-    private static String baseUrl = "https://demoqa.com";
-    private static String browserSize = "1920x1080";
+    private static String browser = System.getProperty("browser", "chrome");
+    private static String browserVersion = System.getProperty("browserVersion", "100.0");
+    private static String remoteBrowser = System.getProperty("remoteBrowser", "selenoid.autotests.cloud");
+    private static String baseUrl = System.getProperty("baseUrl", "https://demoqa.com" );
+    private static String browserSize = System.getProperty("browserSize", "1920x1080" );
+
     @BeforeAll
     public static void beforeAll(){
+
+
         WebDriverManager.chromedriver().setup();
         Configuration.baseUrl = baseUrl;
         Configuration.browserSize = browserSize;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.pageLoadTimeout = 15000;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+
+        Configuration.remote = String.format("https://user1:1234@%s/wd/hub", remoteBrowser);
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
         Configuration.browserCapabilities = capabilities;
 
-//        Configuration.pageLoadTimeout = 15000;
-//        Configuration.browser = "fireFox";
-        Configuration.pageLoadStrategy = "eager";
-//        Configuration.holdBrowserOpen = true;
+
     }
 
     @BeforeEach
     public void setUp(){
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
 
     @AfterEach
     public void afterEach(){
